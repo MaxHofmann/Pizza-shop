e0c45e8ba9bfab52c6eaa79f45b6b14f0386c1ca
MaxHofmann <maksveremey4ik@gmail.com>
2021-09-12 17:12:17 +0300

    back img scss

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { background, logoSvg } from '../assets';
+import { logoSvg } from '../assets';
 
 function Banner() {
   return (
     <div className="banner">
-      <div className="banner__block" style={{ backgroundImage: `url(${background})` }}>
+      <div className="banner__block" >
         <div className="banner__block--wrap">
           <h1 className="notranslate">Pizza shop</h1>
           <img src={logoSvg} alt="logoSvg" />
